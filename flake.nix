61183bbc7a833f30eee43b75c2cb91dab49d2db5
Sulaiman Khan Ghori <sulaiman.ghori@outlook.com>
2025-08-16 14:27:33 -0700

    nixfmt

--- flake.nix
+++ flake.nix
@@ -7,109 +7,122 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }:
-  let
-    system   = "x86_64-linux";
-    hostname = "nixbox";
-    myUser   = "djmango";
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+      hostname = "nixbox";
+      myUser = "djmango";
 
-    pkgs = import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+
+      lib = nixpkgs.lib;
+
+      # Your Neovim config repo (pin to a commit when you can).
+      nvimConfig = pkgs.fetchFromGitHub {
+        owner = "djmango";
+        repo = "dotfiles-nvim";
+        rev = "master";
+      };
+    in
+    {
+      nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          home-manager.nixosModules.home-manager
+          (
+            { pkgs, ... }:
+            {
+              nix.settings.experimental-features = [
+                "nix-command"
+                "flakes"
+              ];
+
+              # Optional: create the user
+              users.users.${myUser} = {
+                isNormalUser = true;
+                extraGroups = [ "wheel" ];
+                openssh.authorizedKeys.keys = [
+                  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK4RmXrS61fSaWojx1aK9oB1N1LzllAkOpcLrcXsUsVz djmango"
+                ];
+              };
+
+              # Core CLI tools at the system level
+              environment.systemPackages = with pkgs; [
+                git
+                openssh
+              ];
+
+              # OpenSSH server
+              services.openssh = {
+                enable = true;
+                ports = [ 22 ];
+                settings = {
+                  PermitRootLogin = "no";
+                  PasswordAuthentication = false;
+                  KbdInteractiveAuthentication = false;
+                };
+              };
+              networking.firewall.allowedTCPPorts = [ 22 ];
+
+              # Home Manager wiring
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.${myUser} =
+                { pkgs, ... }:
+                {
+                  home.stateVersion = "24.05";
+                  programs.home-manager.enable = true;
+
+                  # Neovim (stable)
+                  programs.neovim = {
+                    enable = true;
+                    viAlias = true;
+                    vimAlias = true;
+                    withNodeJs = true;
+                    withPython3 = true;
+                    # package = pkgs.neovim; # default
+                  };
+
+                  # Put your Neovim repo into ~/.config/nvim
+                  xdg.configFile."nvim".source = nvimConfig;
+
+                  # Shell helpers
+                  programs.atuin = {
+                    enable = true;
+                    enableBashIntegration = true;
+                    enableZshIntegration = true;
+                    enableFishIntegration = true;
+                  };
+                  programs.zoxide = {
+                    enable = true;
+                    enableBashIntegration = true;
+                    enableZshIntegration = true;
+                    enableFishIntegration = true;
+                  };
+
+                  # Extra packages
+                  home.packages = with pkgs; [
+                    uv
+                    fd
+                    magic-wormhole
+                    rustc
+                    cargo
+                    rust-analyzer
+                    clippy
+                  ];
+                };
+            }
+          )
+        ];
+      };
     };
-
-    lib = nixpkgs.lib;
-
-    # Your Neovim config repo (pin to a commit when you can).
-    nvimConfig = pkgs.fetchFromGitHub {
-      owner = "djmango";
-      repo  = "dotfiles-nvim";
-      rev   = "master";
-    };
-  in {
-    nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
-      inherit system;
-      modules = [
-        home-manager.nixosModules.home-manager
-        ({ pkgs, ... }: {
-          nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-          # Optional: create the user
-          users.users.${myUser} = {
-            isNormalUser = true;
-            extraGroups = [ "wheel" ];
-          };
-
-          # Core CLI tools at the system level
-          environment.systemPackages = with pkgs; [
-            git
-            openssh
-          ];
-
-          # OpenSSH server
-          services.openssh = {
-            enable = true;
-            ports = [ 22 ];
-            settings = {
-              PermitRootLogin = "no";
-              PasswordAuthentication = false;
-              KbdInteractiveAuthentication = false;
-            };
-          };
-          networking.firewall.allowedTCPPorts = [ 22 ];
-
-          users.users.${myUser}.openssh.authorizedKeys.keys = [
-	    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK4RmXrS61fSaWojx1aK9oB1N1LzllAkOpcLrcXsUsVz djmango"
-          ];
-
-          # Home Manager wiring
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.${myUser} = { pkgs, ... }: {
-            home.stateVersion = "24.05";
-            programs.home-manager.enable = true;
-
-            # Neovim (stable)
-            programs.neovim = {
-              enable = true;
-              viAlias = true;
-              vimAlias = true;
-              withNodeJs = true;
-              withPython3 = true;
-              # package = pkgs.neovim; # default
-            };
-
-            # Put your Neovim repo into ~/.config/nvim
-            xdg.configFile."nvim".source = nvimConfig;
-
-            # Shell helpers
-            programs.atuin = {
-              enable = true;
-              enableBashIntegration = true;
-              enableZshIntegration  = true;
-              enableFishIntegration = true;
-            };
-            programs.zoxide = {
-              enable = true;
-              enableBashIntegration = true;
-              enableZshIntegration  = true;
-              enableFishIntegration = true;
-            };
-
-            # Extra packages
-            home.packages = with pkgs; [
-              uv
-              fd
-              magic-wormhole
-	      rustc
-              cargo
-              rust-analyzer
-              clippy
-            ];
-          };
-        })
-      ];
-    };
-  };
 }
-
