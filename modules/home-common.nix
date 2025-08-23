fdea30e918131a37e5a747379c643e6f2528c708
djmango <sulaiman@x.ai>
2025-08-22 17:35:57 -0700

    Tmux

--- modules/home-common.nix
+++ modules/home-common.nix
@@ -19,6 +19,64 @@
   programs.atuin.enable = true;
   programs.zoxide.enable = true;
 
+  programs.tmux = {
+    enable = true;
+    prefix = "C-a";
+    keyMode = "vi";
+    extraConfig = ''
+      # Set Ctrl+a as prefix (standard tmux default)
+      set -g prefix C-a
+      unbind C-b
+      bind C-a send-prefix
+
+      # Window switching
+      bind -n C-h previous-window
+      bind -n C-l next-window
+
+      # Enable mouse support
+      set -g mouse on
+
+      # don't do anything when a 'bell' rings
+      set -g visual-activity off
+      set -g visual-bell off
+      set -g visual-silence off
+      setw -g monitor-activity off
+      set -g bell-action none
+
+      # clock mode
+      setw -g clock-mode-colour "yellow"
+
+      # copy mode
+      setw -g mode-style "fg=black,bg=red,bold"
+
+      # panes
+      set -g pane-border-style "fg=red"
+      set -g pane-active-border-style "fg=yellow"
+
+      # statusbar
+      set -g status-position bottom
+      set -g status-justify left
+      set -g status-style "fg=red,bg=black"
+
+      set -g status-left ""
+      set -g status-left-length 10
+
+      set -g status-right "#[fg=black,bg=yellow] %Y-%m-%d %H:%M "
+      set -g status-right-length 50
+
+      set-option -g window-status-current-style "fg=black,bg=red"
+      set-option -g window-status-current-format " #I #W #F "
+
+      set-option -g window-status-style "fg=red,bg=black"
+      set-option -g window-status-format " #I #W #F "
+
+      set-option -g window-status-bell-style "fg=yellow,bg=red,bold"
+
+      # messages
+      set -g message-style "fg=yellow,bg=red,bold"
+    '';
+  };
+
   # programs.git = {
   #   enable = true;
   #   userName = "Your Name";
@@ -29,23 +87,14 @@
 
   # Custom packages
   home.packages = with pkgs; [
-    uv # Fast Python package manager
-    ruff # Fast Python linter and formatter
-    fd # Fast file finder
-    rustc # Rust compiler
-    cargo # Rust package manager
-    rust-analyzer # Rust language server
-    clippy # Rust linter
+bun cargo clippy code2prompt fd gh
+
     git
-    gh
+htop magic-wormhole nil nixd nixfmt
+
     rclone
-    htop # System monitor
-    magic-wormhole # Secure file transfer
-    nil # Nix language server
-    nixd
-    code2prompt # Convert codebases to prompts for AI
-    zed-editor # Zed code editor
-    bun
+ruff rust-analyzer rustc tmux uv zed-editor
+
   ];
 
   # Allow unfree packages
